6b6dd00f4cef34d4f47195932f43396acffb3489
zach <zacharymorr@outlook.com>
2024-03-09 22:03:07 -0800

    First Commit!

---
+++ board/unknown/licheepi_nano/boot.cmd
@@ -0,0 +1,4 @@
+setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait earlyprintk
+fatload mmc 0 ${kernel_addr_r} zImage
+fatload mmc 0 ${fdt_addr_r} suniv-f1c100s-licheepi-nano.dtb
+bootz ${kernel_addr_r} - ${fdt_addr_r}
