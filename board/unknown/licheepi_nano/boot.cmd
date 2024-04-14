cf80bde476a88c7b5a09f264c6d289cc1faf8c0e
zach <zacharymorr@outlook.com>
2024-04-13 17:05:44 -0700

    Working wifi with esp32

--- board/unknown/licheepi_nano/boot.cmd
+++ board/unknown/licheepi_nano/boot.cmd
@@ -1,4 +1,4 @@
 setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait earlyprintk
 fatload mmc 0 ${kernel_addr_r} zImage
-fatload mmc 0 ${fdt_addr_r} suniv-f1c100s-licheepi-nano.dtb
+fatload mmc 0 ${fdt_addr_r} licheepi_nano.dtb
 bootz ${kernel_addr_r} - ${fdt_addr_r}
